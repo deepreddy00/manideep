88567d120242498e9eb390b88dab2c3b67020e38
manideepreddyvallapu <manideepreddyvallapu@192.168.0.10>
2019-11-28 13:05:52 +0200

    jk

--- 123445/src/test/java/TestSeleniumdemo/Apptest.java
+++ 123445/src/test/java/TestSeleniumdemo/Apptest.java
@@ -11,8 +11,6 @@ public class Apptest
 
 {
 
-	
-	
 	@Test
 	public void demo() throws Throwable
 	{
@@ -20,6 +18,39 @@ public class Apptest
 		bs.bro("chrome");
 		App demo=new App();
 		demo.goodUSPW("user@phptravels.com", "demouser");
-				}
+			}
+	@Test(priority=1)
+	public void demo1() throws Throwable
+	{
+		Baseclass bs=new Baseclass();
+		bs.bro("chrome");
+		App demo=new App();
+		demo.goodUSPW("mankknds@gmail.com", "demouser");
+			}
+	@Test(priority=2)
+	public void demo3() throws Throwable
+	{
+		Baseclass bs=new Baseclass();
+		bs.bro("chrome");
+		App demo=new App();
+		demo.goodUSPW("user@phptravels.com", "wqqrrq");
+			}
+	@Test(priority=3)
+	public void demo4() throws Throwable
+	{
+		Baseclass bs=new Baseclass();
+		bs.bro("chrome");
+		App demo=new App();
+		demo.goodUSPW("user@p", "dser");
+			}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 			
 }
